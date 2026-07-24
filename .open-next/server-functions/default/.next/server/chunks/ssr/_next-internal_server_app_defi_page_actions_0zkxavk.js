d8b783004e45202fb111f5e7547b4715a63a9a21
contacttradingsetup <contact.tradingsetup@gmail.com>
2026-07-24 07:16:18 +0700

    Revert to simple Next.js setup, remove OpenNext/Wrangler

--- .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_defi_page_actions_0zkxavk.js
+++
@@ -1,3 +0,0 @@
-module.exports=[8554,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_defi_page_actions_0zkxavk.js.map
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Wn9xqOeG+pjL3ShT9K8IvRM6xtKdnu8fXHUuD9Jlbng=\"\n}"
